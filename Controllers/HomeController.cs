0c304d7130569326b653147c5f9a221725671245
WilliamPrakash <willprakash@gmail.com>
2025-04-18 11:10:43 -0400

    Cleaning up unnecessary code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,12 +8,10 @@ namespace MVC_Project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly MVC_ProjectDBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger, MVC_ProjectDBContext dbContext)
+        public HomeController(MVC_ProjectDBContext dbContext)
         {
-            _logger = logger;
             _dbContext = dbContext;
         }
 
@@ -89,42 +87,5 @@ namespace MVC_Project.Controllers
             return RedirectToAction("Expenses");
         }
 
-        [Obsolete("No longer using vanilla C# to access SQL, use Entity Framework instead.")]
-        // Keep for reference
-        public void EstablishSQLConnection()
-        {
-            // Grab Credentials from credentials.json stored on local machine
-            GrabLocalDatabaseCredentials credentialGrabber = new GrabLocalDatabaseCredentials();
-            Dictionary<string,string> credentials = credentialGrabber.OpenLocalAuthFile();
-
-            // Create SQL connection
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = credentials["SQLServer_Win"];
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                conn.ConnectionString = credentials["SQLServer_Mac"];
-            }
-
-            // Attempt to connect/query
-            try
-            {
-                conn.Open();
-                string query = "select * from master.dbo.Employees";
-                var command = new SqlCommand(query, conn);
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine("{0} {1} {2} {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            //conn.Close();
-            
-            Console.WriteLine("");
-        }
     }
 }
